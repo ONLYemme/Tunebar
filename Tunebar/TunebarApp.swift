59a8f0a0c52efcad2babdfb2d68d30bb11ea7a1c
ONLYemme <mattiagokart@gmail.com>
2024-07-22 15:24:44 +0200

    Initial Commit

---
+++ Tunebar/TunebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunebarApp.swift
+//  Tunebar
+//
+//  Created by Mattia on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TunebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
